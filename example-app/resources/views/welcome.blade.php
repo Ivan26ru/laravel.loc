080bb9e576e641d9864939e33b01fa21f9f554d1
Ivan <it-1ivan@ya.ru>
2023-06-05 02:10:38 +0500

    html gallery

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,18 +1,29 @@
 @extends('layout')
 
 @section('content')
+    <style>
+        button.my-button {
+            width: 100%;
+            margin-top: 5px;
+        }
 
+        .gallery-item{
+            margin: 10px 0;
+        }
+    </style>
     <div class="container">
         <h1 align="center">My Gallery</h1>
         <div class="row">
             <?php for ($i = 0;
-                $i < 4;
+                $i < 8;
                 $i++) { ?>
-            <div class="col-md-3">
+            <div class="col-md-3 gallery-item">
                 <div>
                     <img src="/image.jpg" alt="" class="img-thumbnail">
                 </div>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa dolorem ipsum quas! Dolores illum inventore quis sit! Fuga, quam velit.
+                <button type="button" class="my-button btn btn-info">Info</button>
+                <button type="button" class="my-button btn btn-warning">Warning</button>
+                <button type="button" class="my-button btn btn-danger">Danger</button>
             </div>
             <?php } ?>
         </div>
