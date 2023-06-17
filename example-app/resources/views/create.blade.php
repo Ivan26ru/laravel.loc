725ef773f95326b56fdde7b6012c6e600828d085
Ivan <it-1ivan@ya.ru>
2023-06-17 10:55:38 +0500

    Laravel l7 use get_class_methods()

--- example-app/resources/views/create.blade.php
+++ example-app/resources/views/create.blade.php
@@ -5,9 +5,10 @@
         <div class="row">
             <div class="col-md-5">
                 <h1>Add Image</h1>
-                <form action="" method="post">
+                <form action="/store" method="post" enctype="multipart/form-data">
+                    {{csrf_field()}}
                     <div class="form-control">
-                        <input type="file" name="" id="">
+                        <input type="file" name="image">
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,14 +24,20 @@ Route::get('/about', function () {
     return view('about');
 });
 
-Route::get('/create', function (){
+Route::get('/create', function () {
     return view('create');
 });
 
-Route::get('/show', function (){
+Route::post('/store', function (Request $request) {
+    $image = $request->file('image');
+//        dd($image->getSize());
+    dd(get_class_methods($image));
+});
+
+Route::get('/show', function () {
     return view('show');
 });
 
-Route::get('/edit', function (){
+Route::get('/edit', function () {
     return view('edit');
 });
