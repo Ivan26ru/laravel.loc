feb05a381d68164db84bc7aa009d0edc9300e903
Ivan <it-1ivan@ya.ru>
2023-09-19 11:44:14 +0500

    Laravel default data

---
+++ example-app/app/Models/Image.php
@@ -0,0 +1,13 @@
+<?php
+//Eloquent https://laravel.com/docs/10.x/eloquent
+//php artisan make:model Image
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+    protected $guarded = ['created_at', 'updated_at'];
+}
