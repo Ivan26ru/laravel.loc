7a8b56698eb45a97879b00a4bc1ddbdfd589afa9
Ivan <it-1ivan@ya.ru>
2023-09-22 13:39:10 +0500

    Laravel 31 update posts

--- example-app/database/migrations/2023_09_22_020242_create_posts_table.php
+++ example-app/database/migrations/2023_09_22_020242_create_posts_table.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->string('slug');
             $table->text('content');
@@ -27,8 +25,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('posts');
     }
 };
