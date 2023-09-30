a664b8b4e4a76942141db409789bd207894af9a2
Ivan <it-1ivan@ya.ru>
2023-10-01 01:21:50 +0500

    Laravel 32 get post for User create

--- example-app/app/Http/Controllers/PostController.php
+++ example-app/app/Http/Controllers/PostController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function posts() {
-        $posts = Post::paginate(5);
-//        $posts = Post::simplePaginate(5);
-        return view('posts', ['posts'=> $posts]);
+        //        $posts = Post::paginate(5);
+        //        $posts = Post::simplePaginate(5);
+
+        //        $posts = Post::first();
+        //        dd($posts->user);
+
+        $user = User::find(7);
+        dd($user->post()->get());
+
+        //        $user = User::find(7);
+        //        dd($user->post()->get());
+        return view('posts', ['posts' => $posts]);
     }
 
 
--- example-app/app/Models/Post.php
+++ example-app/app/Models/Post.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected function user(): BelongsTo {
+        return $this->belongsTo('App\Models\User');
+    }
 }
